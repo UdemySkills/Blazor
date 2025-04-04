e1eed53094a11e70933041cfb8fd19bfe2cc53fd
venkateshellur <venkatesh.ellur@gmail.com>
2025-04-03 22:32:54 -0500

    Sec 3 - 12. Binding Data to Dropdown
    https://slk.udemy.com/course/introduction-to-aspnet-core-x/learn/lecture/45107093#overview

--- Practice/LearningBlazor/Models/Product.cs
+++ Practice/LearningBlazor/Models/Product.cs
@@ -8,4 +8,5 @@ public class Product
     public string Name { get; set; }
     public bool IsActive { get; set; }
     public int Price { get; set; }
+    public IEnumerable<Product_Prop> ProductProperties { get; set; }
 }
